6cc0e4005ca4fa7e11e2ecce44cb1283983f3e72
gilch <githuub.gilch@xoxy.net>
2015-09-04 17:43:17 -0600

    more doctests better py_call

--- expressive/stack.py
+++ expressive/stack.py
@@ -1,38 +1,53 @@
 from expressive.core import Tuple
-from collections import deque
+from collections import deque, Mapping
 
 
 class Stack:
-    def __init__(self, *args, head=None):
-        self._head = head
+    def __init__(self, *args, rest=None):
+        self.head = rest
         for w in args:
             if hasattr(w, '__stack_word__'):
-                self._head = w(self)._head
+                self.head = w(self).head
             else:
-                self._head = (self._head, w)
+                self.head = (self.head, w)
 
     def __iter__(self):
+        """
+        Stack returns its elements in the reverse of insertion order,
+        hence the name.
+        >>> list(Stack(1,2,3))
+        [3, 2, 1]
+        """
         def it():
-            head = self._head
+            head = self.head
             while head:
                 yield head[1]
                 head = head[0]
         return it()
 
     def __repr__(self):
-        return 'Stack%s' % repr(tuple(reversed(tuple(self))))
-        # return 'Stack(head=%s)' % repr(self._head)
+        return 'Stack' + repr(tuple(reversed(tuple(self))))
+        # return 'Stack(head=%s)' % repr(self.head)
 
     def push(self, *args):
         """
+        Add an element on top of the stack.
         >>> Stack().push(1)
         Stack(1,)
-        >>> Stack().push(1, 2, 3, 4, 5)
-        Stack(1, 2, 3, 4, 5)
-        >>> Stack(1, 2).push(3).push(4, 5)
-        Stack(1, 2, 3, 4, 5)
+
+        Returns the new stack.
+        >>> Stack().push(1).push(2).push(3).push(4)
+        Stack(1, 2, 3, 4)
+
+        Same as above
+        >>> Stack().push(1, 2, 3, 4)
+        Stack(1, 2, 3, 4)
+
+        Pushing a stack word executes it
+        >>> Stack().push(1, dup, 2)
+        Stack(1, 1, 2)
         """
-        return Stack(*args, head=self._head)
+        return Stack(*args, rest=self.head)
 
     def pop(self, depth=1):
         """
@@ -55,15 +70,15 @@ class Stack:
         Stack underflow
         """
         xs = deque()
-        head = self._head
+        head = self.head
         try:
             while depth > 0:
                 depth -= 1
                 xs.appendleft(head[1])
                 head = head[0]
-        except TypeError:
-            raise IndexError('Stack underflow')
-        return Tuple(Stack(head=head), *xs)
+        except TypeError as te:
+            raise IndexError('Stack underflow') from te
+        return Tuple(Stack(rest=head), *xs)
 
     def peek(self, depth=None):
         """
@@ -89,17 +104,36 @@ class Stack:
         return res
 
 
-
-
 def stack_word(func):
+    if hasattr(func, '__doc__') and func.__doc__ is not None:
+        func.__doc__ = 'stack word: '+func.__doc__
     func.__stack_word__ = None
     return func
 
 
 @stack_word
-def call(stack):
-    stack, func, args, kwargs = stack.pop(3)
-    return stack.push(func(*args,**kwargs))
+def py_call(stack):
+    """
+    calls an ordinary Python function using the stack.
+    (The print function returns None)
+    >>> Stack((1,2,3),print,py_call)
+    1 2 3
+    Stack(None,)
+
+    Keywords are also supported with a dictionary
+    >>> Stack((1,2,3),dict(sep=':'),print,py_call)
+    1:2:3
+    Stack(None,)
+
+    Use an empty tuple for no arguments
+    >>> Stack((),dict,py_call)
+    Stack({},)
+    """
+    stack, kwargs, func = stack.pop(2)
+    if isinstance(kwargs, Mapping):
+        stack, args = stack.pop()
+        return stack.push(func(*args, **kwargs))
+    return stack.push(func(*kwargs))  # kwargs was just args
 
 
 @stack_word
@@ -109,8 +143,9 @@ def wrap(stack):
     return stack, args
 
 
-def op(func):
+def op(func, depth=2):
     """
+    converts a binary Python function into a stack word
     >>> from operator import add, mul
     >>> Stack(2, 3, op(add))
     Stack(5,)
@@ -118,12 +153,20 @@ def op(func):
     6
     >>> Stack(4, 2, 3, op(add), op(mul)).peek()
     20
+
+    you can specify a different arity
+    >>> Stack(1,2,3,4,5,op(Tuple,4))
+    Stack(1, (2, 3, 4, 5))
+
+    but the default assumption is two arguments
+    >>> Stack(1,2,3,op(Tuple))
+    Stack(1, (2, 3))
     """
     @stack_word
-    def binary_operator(stack):
-        stack, x, y = stack.pop(2)
-        return stack.push(func(x, y))
-    return binary_operator
+    def op_word(stack):
+        stack, *args = stack.pop(depth)
+        return stack.push(func(*args))
+    return op_word
 
 
 def op1(func):
